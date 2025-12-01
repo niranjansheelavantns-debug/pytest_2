0e7d5e9d8ec597e0399cf1cfda4e16a07098bfdb
Niranjan  sheelavant <niranjansheelavant.ns@gmail.com>
2025-12-01 16:24:54 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,18 +1,18 @@
-def employee_info(name,emp_id,department,salary):
-    result=(
-        f"Employee Name:{name}\n"
-        f"Employee ID:{emp_id}\n"
-        f"department:{department}\n"
-        f"Employee Salary:{salary}"
+def employee_info(name, emp_id, department, salary):
+    result = (
+        f"Employee Name: {name}\n"
+        f"Employee ID: {emp_id}\n"
+        f"Department: {department}\n"
+        f"Salary: {salary}"
     )
     return result
 
-if __name__=="__main__":
-    #simple input(you cab change)
-    name = "Alice"
-    emp_id = "e101"
-    department = "IT"
-    salary = 55000
+if __name__ == "__main__":
+   
+        name = "Alice\n"
+        emp_id = "E101\n"
+        department = "IT\n"
+        salary = 55000
 
-    print(employee_info(name, emp_id, department, salary))
-    
+        print(employee_info(name, emp_id, department, salary))
+
