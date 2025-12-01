050135001529abfd29f5470234ada66ca0e103fc
Niranjan  sheelavant <niranjansheelavant.ns@gmail.com>
2025-12-01 15:52:17 +0530

    Update test_employee.py

--- test_employee.py
+++ test_employee.py
@@ -4,8 +4,8 @@ def test_employee_details():
     expected_output=(
         "Employee Name: Alice\n"
         "Employee ID: e101\n"
-        "department = IT\n"
-        "salary = 55000"
+        "department : IT\n"
+        "salary : 55000"
     )
 
     assert employee_details("Alice","e101","IT",55000) == expected_output
